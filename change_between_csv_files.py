8fd4ea24963cd73768a8845e052e258ad1a6a54d
Ajay Sarode <54261918+ajay-sarode-intel@users.noreply.github.com>
2019-10-11 10:58:12 +0530

    Update change_between_csv_files.py

--- change_between_csv_files.py
+++ change_between_csv_files.py
@@ -1,6 +1,8 @@
 from itertools import izip_longest
 import csv
 
+# This part of code open and reads the two input csv files
+
 t1 = open('/nfs/sc/disks/sc_rtl_users_0002/asarode/script_automation/old_file.csv', 'r')
 t2 = open('/nfs/sc/disks/sc_rtl_users_0002/asarode/script_automation/new_file.csv', 'r')
 
@@ -8,39 +10,41 @@ with open('/nfs/sc/disks/sc_rtl_users_0002/asarode/script_automation/old_file.cs
     fileone = t1.readlines()
     filetwo = t2.readlines()
 
-# This part of code copies old comments and keep old violations     
+# This part of code copies old violations with comments and keep violation as it is if previous comment was not present     
 
 with open('update.csv', 'w') as outFile:
+    outFile.write('*** Old violations that has not resolved ***\n')
     for line in filetwo:
         array2 = line.split(',')
-        #print(array2)
-        #print array2[0] is violation
-        #print array2[1] is comment
         for line in fileone:
             array1 = line.split(',')
-            #print(arra12)
-            #print array1[0] is violation
-            #print array1[1] is comment
-            data = [array1[0], array1[1]]
-
             if array2[0] == array1[0] :
-                outFile.write(",".join(data))                
+                outFile.write(",".join(array1)) 
 
-# This part of code writes only new violations which are absent in old file    
+# This part of code writes only new violations which are not present in old file    
 
 with open('update.csv', 'a') as outFile:
+    outFile.write('*** New Violations ***\n')
     for line in filetwo:
         array2 = line.split(',')
-        #print array2[0] is violation
-        #print array2[1] is comment
         s=0   # s is initialized as zero
         for line in fileone:
             array1 = line.split(',')
-            #print(arra12)
-            #print array1[0] is violation
-            #print array1[1] is comment
-
             if array2[0] == array1[0] :
                 s = s+1 # s incrments only if violation from two files matches
         if s==0 :
             outFile.write(",".join(array2))
+
+# This part of code writes violations which are resolved and not present in old file 
+
+with open('update.csv', 'a') as outFile:
+    outFile.write('*** Old violations that has resolved ***\n')
+    for line in fileone:
+        array1 = line.split(',')
+        p=0   # p is initialized as zero
+        for line in filetwo:
+            array2 = line.split(',')
+            if array1[0] == array2[0] :
+                p = p+1 # p incrments only if violation from two files matches
+        if p==0 :
+            outFile.write(",".join(array1))
